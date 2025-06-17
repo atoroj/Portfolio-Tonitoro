936234a1368591e4984a1ddc1267e337ff6ee345
Antonio Toro Jaén <atorojaen@gmail.com>
2025-06-17 22:35:20 +0200

    Commit portfolio

---
+++ src/i18n/redirectToLang.ts
@@ -0,0 +1,24 @@
+import { defaultLang, languages } from "../i18n/ui";
+
+/**
+ * Recibe el header `accept-language` y devuelve una Response con redirección
+ */
+export function redirectToPreferredLang(acceptLangHeader: string | null): Response {
+  const supportedLangs = Object.keys(languages); // ["en", "es"]
+  const acceptLang = acceptLangHeader?.toLowerCase() ?? "";
+  let redirectLang = defaultLang;
+
+  for (const lang of supportedLangs) {
+    if (acceptLang.startsWith(lang)) {
+      redirectLang = lang;
+      break;
+    }
+  }
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: `/${redirectLang}`,
+    },
+  });
+}
---
+++ src/i18n/ui.ts
@@ -0,0 +1,47 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+}
+
+export const defaultLang = 'es';
+export const showDefaultLang = true;
+export const ui = {
+    en:{
+        'work': 'Work experience',
+        'education': 'Education',
+        'about': 'About me',
+        'projects': 'Projects',
+        'cv': 'View CV',
+        'contact': 'Contact',
+        work_label:{
+            
+        },
+        education_label:{
+
+        },
+        projects_label:{
+        },
+        about_label:{
+
+        },
+    },
+    es:{
+        'work': 'Experiencia laboral',
+        'education': 'Educación',
+        'about': 'Sobre mí',
+        'projects': 'Proyectos',
+        'cv': 'Ver CV',
+        'contact': 'Contacto',
+        work_label:{
+            
+        },
+        education_label:{
+
+        },
+        projects_label:{
+        },
+        about_label:{
+
+        },
+    }
+} as const;
