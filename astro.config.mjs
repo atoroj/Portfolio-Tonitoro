d7c43d71cabc355b56328898563013f0f4a39ddf
Antonio Toro Jaén <atorojaen@gmail.com>
2025-06-18 12:10:50 +0200

    404 redirije a main y cuando se abre español por defecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  redirects: {
+    '/': {
+      status: 308,
+      destination: '/es',
+    },
+  },
 });
